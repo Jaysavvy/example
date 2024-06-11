41dfeb3e10aba4ff40b1c70c5e6ebf49e06787d0
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2024-06-11 17:33:50 -0400

    Setup form to create new jobs

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,6 +2,7 @@
     <x-slot:heading>
         Job Listings
     </x-slot:heading>
+
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
@@ -9,6 +10,7 @@
                 <div class="font-bold text-blue-600 text-sm">
                     {{ $job->employer->name }}
                 </div>
+
                 <div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,24 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(3);
+    $jobs = Job::with('employer')->paginate(7);
 
 
-    return view(
-        'jobs',
-        [
-            'jobs' => $jobs
-        ]
-    );
+    return view('jobs.index', [
+        'jobs' => $jobs
+    ]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
+
 
 
 Route::get('/contact', function () {
